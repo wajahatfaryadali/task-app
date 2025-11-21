a4f7d1bcd5c1e2af8aa96266f1ba45b9cd3c4e22
Wajahat Ali <wajahatfaryadali@gmail.com>
2025-11-21 22:06:05 +0500

    adding buttons on mobile view

--- app/components/searchBar/SearchBar.tsx
+++ app/components/searchBar/SearchBar.tsx
@@ -45,13 +45,18 @@ const SearchBar = () => {
       className="relative w-full rounded-[10px] bg-white px-[15px] pt-6 pb-2.5"
     >
       <div className="hidden lg:block">
-        <VenueOrVendor />
+        <div className="absolute left-1/2 flex w-[264px] -translate-x-1/2 -top-[40px] items-center justify-center rounded-[10px] bg-white px-6 py-2 text-black shadow-lg">
+          <VenueOrVendor />
+        </div>
       </div>
       <div className="grid gap-2 md:gap-6 md:grid-cols-[repeat(3,minmax(0,1fr))_auto]">
+        <div className="md:hidden">
+          <VenueOrVendor />
+        </div>
         <SelectMenu label="Where" options={whereOptions} defaultValue="dubai" />
-        <hr className="border-[#E0E0E0] md:hidden"/>
+        <hr className="border-[#E0E0E0] md:hidden" />
         <SelectMenu label="When" options={whenOptions} defaultValue="anytime" />
-        <hr className="border-[#E0E0E0] md:hidden"/>
+        <hr className="border-[#E0E0E0] md:hidden" />
         <SelectMenu
           label="Guests"
           options={guestOptions}
